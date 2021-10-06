39930d593628a7f0ed28f5117ec828ce80ee09cd
the-ahuja-lab <60312173+the-ahuja-lab@users.noreply.github.com>
2021-10-06 15:38:15 +0530

    Delete Main_pipeline/Code directory

--- Main_pipeline/Code/Data Processing/bash03_velocyto.sh
+++
@@ -1,2 +0,0 @@
-#! usr/bin/bash
-velocyto run10x -m /home/sidrah19220/nmd/mouse/mm10_rmsk.gtf /home/sidrah19220/nmd/mouse/md1/SRR12545952 /home/sidrah19220/nmd/mouse/refdata-gex-mm10-2020-A/genes/genes.gtf 
